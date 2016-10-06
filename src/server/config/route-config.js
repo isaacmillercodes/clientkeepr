f7fb313e7a4113e4580f1ace4a462f4b06120199
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2016-10-06 10:25:05 -0600

    set up file structure

--- src/server/config/route-config.js
+++ src/server/config/route-config.js
@@ -5,11 +5,12 @@
   routeConfig.init = function (app) {
 
     // *** routes *** //
-    const routes = require('../routes/index');
+    const routes = require('../routes/home');
+    const clientRoutes = require('../routes/clients');
 
     // *** register routes *** //
     app.use('/', routes);
-
+    app.use('/clients', clientRoutes);
   };
 
 })(module.exports);
